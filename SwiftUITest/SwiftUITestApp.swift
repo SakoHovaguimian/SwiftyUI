c5bc48f2afdf8798a54afa083064dcebd2e31e43
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2023-06-11 08:42:44 -0700

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Sako Hovaguimian on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
