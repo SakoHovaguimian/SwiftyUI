2c8905302d127f3003b55289f1f8ceead4a8d627
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2023-06-19 07:54:16 -0700

    added some file structure and a playgorundView

---
+++ SwiftUITest/Extensions/Extension+Double.swift
@@ -0,0 +1,28 @@
+//
+//  Extension+Double.swift
+//  SwiftUITest
+//
+//  Created by Sako Hovaguimian on 6/19/23.
+//
+
+import Foundation
+
+//extension Double {
+//    
+//    func round(to places: Int) -> Double {
+//        
+//        let multiple = pow(10, Double(places))
+//        print(multiple)
+//        print("result: , \((multiple * self).rounded() / multiple)")
+//        return (multiple * self).rounded() / multiple
+//        
+//    }
+//    
+//    func truncate(to places: Int) -> Double {
+//        
+//        let multiple = pow(10, Double(places))
+//        return (multiple * self) / multiple
+//        
+//    }
+//    
+//}
