5de5d91a4ac7c983cafe043bb79f180d8902e683
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2023-07-07 15:30:37 -0700

    animated search bar

--- SwiftUITest/AnimatedSearchBarView.swift
+++ SwiftUITest/AnimatedSearchBarView.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 
-// NOTES
-/// ZStack trialing
-/// Animate width of whole container and corner radius of whole container
-/// Add overlay to whole container
-///
+// Clean up
+// image param
+// height
+// padding
+// width
+// binding text
+// Trailing or leading for animation
+
 struct AnimatedSearchBarView: View {
     
-    @State private var text: String = ""
+    @State private var text: String = "" // Make this binding
     @State private var isExpanded: Bool = false
+    @State private var opacity: CGFloat = 0
     
     var body: some View {
         
@@ -35,6 +39,7 @@ struct AnimatedSearchBarView: View {
                         .scaledToFill()
                         .foregroundStyle(.white)
                         .frame(width: 24, height: 24)
+                        .padding(.vertical, 12)
                     
                     Spacer()
                     
@@ -47,25 +52,34 @@ struct AnimatedSearchBarView: View {
                     .foregroundColor(.black)
                     .fontWeight(.semibold)
                     .font(.body)
+                    .padding(.vertical, 12)
+                    .padding(.leading, 16)
                     .fontDesign(.rounded)
                     .background {
                         Color.gray.opacity(0.1)
                     }
+                    .opacity(self.opacity)
                     
                 }
                 
             }
-            .padding(.vertical, 12)
-            .frame(width: self.isExpanded ? UIScreen.main.bounds.width : 48)
             .overlay(
-                RoundedRectangle(cornerRadius: self.isExpanded ? 12 : 12)
+                RoundedRectangle(cornerRadius: 12)
                     .stroke(Color.blue, lineWidth: 2)
             )
+            .frame(width: self.isExpanded ? UIScreen.main.bounds.width : 48)
             
         }
         .onTapGesture {
+            
             withAnimation {
                 self.isExpanded.toggle()
+            }
+            
+        }
+        .onChange(of: self.isExpanded) { oldValue, newValue in
+            withAnimation(.spring().delay(0.2)) {
+                self.opacity = newValue ? 1 : 0
             }
         }
         
