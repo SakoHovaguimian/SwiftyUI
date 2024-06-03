152bbbdf74abd71efd2cb48f5ffea4e6e5cd1dc7
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2024-06-03 15:15:40 -0700

    Initial Commit

---
+++ SwiftyUI/SwiftyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyUIApp.swift
+//  SwiftyUI
+//
+//  Created by Sako Hovaguimian on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
