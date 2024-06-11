13c1548adf20f83f49027ba263b1c766833f3bd5
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2024-06-11 11:02:39 -0700

    image style

--- SwiftyUI/ios18TabBar.swift
+++ SwiftyUI/ios18TabBar.swift
@@ -5,110 +5,112 @@
 //  Created by Sako Hovaguimian on 6/11/24.
 //
 
-import SwiftUI
+// Won't Compile. Blaming BETA
 
-enum SomeDestination: Hashable {
-    case home
-    case search
-    case settings
-    case trends
-}
-
-struct NewTabBar: View {
-    
-    @State private var selection: SomeDestination = .home
-    
-    var body: some View {
-        
-        // Should we bind to selection?
-        
-        TabView {
-            
-            Tab("home", systemImage: "home", value: ) {
-                TabViewHomeView()
-            }
-            
-            Tab("search", systemImage: "search", value: .search) {
-                TabViewSearchView()
-            }
-            
-            TabSection("Other") {
-                
-                Tab("trends", systemImage: "trends", value: .trends) {
-                    TabViewTrendsView()
-                }
-                Tab("settings", systemImage: "settings", value: .settings) {
-                    TabViewSettingsView()
-                }
-                
-            }
-            .tabViewStyle(.sidebarAdaptable)
-        }
-        
-    }
-    
-}
-
-struct TabViewHomeView: View {
-    
-    var body: some View {
-        
-        ZStack {
-            
-            Color.darkBlue
-                .ignoresSafeArea()
-            
-        }
-        
-    }
-    
-}
-
-struct TabViewSearchView: View {
-    
-    var body: some View {
-        
-        ZStack {
-            
-            Color.darkGreen
-                .ignoresSafeArea()
-            
-        }
-        
-    }
-    
-}
-
-struct TabViewTrendsView: View {
-    
-    var body: some View {
-        
-        ZStack {
-            
-            Color.darkPurple
-                .ignoresSafeArea()
-            
-        }
-        
-    }
-    
-}
-
-struct TabViewSettingsView: View {
-    
-    var body: some View {
-        
-        ZStack {
-            
-            Color.darkYellow
-                .ignoresSafeArea()
-            
-        }
-        
-    }
-    
-}
-
-#Preview {
-    NewTabBar()
-}
+//import SwiftUI
+//
+//enum SomeDestination: Hashable {
+//    case home
+//    case search
+//    case settings
+//    case trends
+//}
+//
+//struct NewTabBar: View {
+//
+//    @State private var selection: SomeDestination = .home
+//    
+//    var body: some View {
+//        
+//        // Should we bind to selection?
+//        
+//        TabView {
+//            
+//            Tab("home", systemImage: "home", value: ) {
+//                TabViewHomeView()
+//            }
+//            
+//            Tab("search", systemImage: "search", value: .search) {
+//                TabViewSearchView()
+//            }
+//            
+//            TabSection("Other") {
+//                
+//                Tab("trends", systemImage: "trends", value: .trends) {
+//                    TabViewTrendsView()
+//                }
+//                Tab("settings", systemImage: "settings", value: .settings) {
+//                    TabViewSettingsView()
+//                }
+//                
+//            }
+//            .tabViewStyle(.sidebarAdaptable)
+//        }
+//        
+//    }
+//    
+//}
+//
+//struct TabViewHomeView: View {
+//    
+//    var body: some View {
+//        
+//        ZStack {
+//            
+//            Color.darkBlue
+//                .ignoresSafeArea()
+//            
+//        }
+//        
+//    }
+//    
+//}
+//
+//struct TabViewSearchView: View {
+//    
+//    var body: some View {
+//        
+//        ZStack {
+//            
+//            Color.darkGreen
+//                .ignoresSafeArea()
+//            
+//        }
+//        
+//    }
+//    
+//}
+//
+//struct TabViewTrendsView: View {
+//    
+//    var body: some View {
+//        
+//        ZStack {
+//            
+//            Color.darkPurple
+//                .ignoresSafeArea()
+//            
+//        }
+//        
+//    }
+//    
+//}
+//
+//struct TabViewSettingsView: View {
+//    
+//    var body: some View {
+//        
+//        ZStack {
+//            
+//            Color.darkYellow
+//                .ignoresSafeArea()
+//            
+//        }
+//        
+//    }
+//    
+//}
+//
+//#Preview {
+//    NewTabBar()
+//}
