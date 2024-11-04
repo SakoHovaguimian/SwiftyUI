44eb4135d6cb186775e836eaec65c9c2a938bc48
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2024-11-04 10:47:01 -0800

    added views

--- SwiftyUI/UI/iOS 18/iOS18Subviews.swift
+++ SwiftyUI/UI/iOS 18/iOS18Subviews.swift
@@ -7,74 +7,74 @@
 
 import SwiftUI
 
-struct SubviewTestView<Content: View>: View {
-    
-    @ViewBuilder var content: Content
-    
-    var body: some View {
-        
-        VStack(alignment: .leading) {
-            
-            Group(subviewsOf: content) { subviews in
-                
-                VStack(alignment: .leading) {
-                    
-                    if !subviews.isEmpty {
-                        let _ = print("Less than 1 subview")
-                        subviews[0]
-                    }
-                    
-                    if subviews.count > 1 {
-                        let _ = print("More than 1 subview")
-                        subviews[1]
-                    }
-                    
-                }
-                
-                if subviews.count > 2 {
-                    
-                    VStack {
-                        subviews[2...]
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        
-    }
-    
-}
-
-#Preview {
-    
-    SubviewTestView {
-        
-        HStack {
-            
-            // Note each stack is it's own count
-            Label("Person", systemImage: "person.fill")
-                .foregroundStyle(.brandPink.gradient)
-            
-        }
-        
-        HStack {
-            
-            // Note each stack is it's own count
-            Label("Money", systemImage: "dollarsign.square.fill")
-                .foregroundStyle(.brandPink.gradient)
-            
-        }
-        
-        HStack {
-            
-            // Note each stack is it's own count
-            Label("Home", systemImage: "house.fill")
-                .foregroundStyle(.brandPink.gradient)
-            
-        }
-        
-    }
-    
-}
+//struct SubviewTestView<Content: View>: View {
+//    
+//    @ViewBuilder var content: Content
+//    
+//    var body: some View {
+//        
+//        VStack(alignment: .leading) {
+//            
+//            Group(sections: content) { subviews in
+//                
+//                VStack(alignment: .leading) {
+//                    
+//                    if !subviews.isEmpty {
+//                        let _ = print("Less than 1 subview")
+//                        subviews[0]
+//                    }
+//                    
+//                    if subviews.count > 1 {
+//                        let _ = print("More than 1 subview")
+//                        subviews[1]
+//                    }
+//                    
+//                }
+//                
+//                if subviews.count > 2 {
+//                    
+//                    VStack {
+//                        subviews[2...]
+//                    }
+//                    
+//                }
+//                
+//            }
+//            
+//        }
+//        
+//    }
+//    
+//}
+//
+//#Preview {
+//    
+//    SubviewTestView {
+//        
+//        HStack {
+//            
+//            // Note each stack is it's own count
+//            Label("Person", systemImage: "person.fill")
+//                .foregroundStyle(.brandPink.gradient)
+//            
+//        }
+//        
+//        HStack {
+//            
+//            // Note each stack is it's own count
+//            Label("Money", systemImage: "dollarsign.square.fill")
+//                .foregroundStyle(.brandPink.gradient)
+//            
+//        }
+//        
+//        HStack {
+//            
+//            // Note each stack is it's own count
+//            Label("Home", systemImage: "house.fill")
+//                .foregroundStyle(.brandPink.gradient)
+//            
+//        }
+//        
+//    }
+//    
+//}
