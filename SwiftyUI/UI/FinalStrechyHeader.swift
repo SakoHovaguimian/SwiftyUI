fb1953efa1b33626bd91a2d4726e6686c483796b
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2024-12-08 08:13:42 -0800

    mirror + more

--- SwiftyUI/UI/FinalStrechyHeader.swift
+++ SwiftyUI/UI/FinalStrechyHeader.swift
@@ -8,36 +8,47 @@
 import SwiftUI
 
 struct SafeAreaInsetsKey: PreferenceKey {
+    
     static let defaultValue = EdgeInsets()
+    
     static func reduce(value: inout EdgeInsets, nextValue: () -> EdgeInsets) {
         value = nextValue()
     }
+    
 }
 
 extension View {
+    
     /// The scroll offset for the scroll view this modifier is attached to.
     /// - Parameter offset: The current offset needed to support stretching. Positive values are ignored and returned as 0 as we only need negative values (scrolling up beyond the top).
     func scrollOffset(_ offset: Binding<CGFloat>) -> some View {
+        
         onScrollGeometryChange(for: CGFloat.self) { geo in
             min(0, geo.contentOffset.y)
         } action: { oldValue, newValue in
             offset.wrappedValue = newValue
         }
+        
     }
+    
 }
 
 extension View {
+    
     /// Get the safe area insets for the view this modifier is attached to.
     /// - Parameter safeInsets: The safe area insets for this view.
     func getSafeAreaInsets(_ safeInsets: Binding<EdgeInsets>) -> some View {
+        
         background(
+            
             GeometryReader { proxy in
                 Color.clear
                     .preference(key: SafeAreaInsetsKey.self, value: proxy.safeAreaInsets)
+                
             }
-                .onPreferenceChange(SafeAreaInsetsKey.self) { value in
-                    safeInsets.wrappedValue = value
-                }
+            .onPreferenceChange(SafeAreaInsetsKey.self) { value in
+                safeInsets.wrappedValue = value
+            }
         )
     }
 }
@@ -45,74 +56,87 @@ extension View {
 public struct StretchyHeaderScrollView<Content: View, Header: View, HeaderBackground: View>: View {
     
     /// The scroll view content following the header view.
-    @ViewBuilder
-    public var content: Content
+    @ViewBuilder public var content: Content
     
     /// The header background.
-    @ViewBuilder
-    public var headerBackground: HeaderBackground
+    @ViewBuilder public var headerBackground: HeaderBackground
     
     /// The header content layered above `headerBackground`.
-    @ViewBuilder
-    public var headerContent: Header
+    @ViewBuilder public var headerContent: Header
     
     @State private var topOffset: CGFloat = 0
     @State private var safeAreaInsets: EdgeInsets = .init()
     
-    public init(
-        @ViewBuilder content: () -> Content,
-        @ViewBuilder headerBackground: () -> HeaderBackground,
-        @ViewBuilder headerContent: () -> Header
-    ) {
+    public init(@ViewBuilder content: () -> Content,
+                @ViewBuilder headerBackground: () -> HeaderBackground,
+                @ViewBuilder headerContent: () -> Header) {
+        
         self.content = content()
         self.headerBackground = headerBackground()
         self.headerContent = headerContent()
+        
     }
     
     public var body: some View {
+        
         ScrollView {
-            HeaderContainer(topOffset: -topOffset, safeAreaTop: safeAreaInsets.top) {
-                headerContent
-            } headerBackground: {
-                headerBackground
+            
+            VStack(spacing: 0) {
+                
+                HeaderContainer(topOffset: -topOffset, safeAreaTop: safeAreaInsets.top) {
+                    headerContent
+                } headerBackground: {
+                    headerBackground
+                }
+                
+                content
+                    .frame(maxWidth: .infinity)
+                    .offset(y: topOffset)
+                
             }
             
-            content
-                .frame(maxWidth: .infinity)
-                .offset(y: topOffset)
         }
         .ignoresSafeArea()
         .scrollOffset($topOffset)
         .getSafeAreaInsets($safeAreaInsets)
+        
     }
+    
 }
 
 fileprivate struct HeaderContainer<Content: View, HeaderBackground: View>: View {
+    
     let topOffset: CGFloat
     let safeAreaTop: CGFloat
-
+    
     var content: () -> Content
     var headerBackground: () -> HeaderBackground
-
+    
     private var scaleFactor: CGFloat {
-        print(1 + (topOffset / 2500))
-        return max(1, 1 + (topOffset / 2500)) // Adjust 200 to control scaling sensitivity
+        print(1 + (topOffset / 4000))
+        return max(1, 1 + (topOffset / 4000)) // Adjust 200 to control scaling sensitivity
     }
-
+    
     var body: some View {
-        VStack(spacing: 0) {
+        
+        VStack(spacing: Spacing.none.value) {
+            
             Spacer(minLength: topOffset + safeAreaTop)
-
             content()
+            
         }
         .background(
+            
             headerBackground()
-                .scaleEffect(scaleFactor, anchor: .top)
+                .scaleEffect(scaleFactor, anchor: .center)
                 .frame(maxWidth: .infinity)
+            
         )
         .clipped()
         .offset(y: -topOffset)
+        
     }
+    
 }
 
 #Preview {
@@ -125,6 +149,9 @@ fileprivate struct HeaderContainer<Content: View, HeaderBackground: View>: View 
                 .fill(.indigo)
                 .frame(height: 100)
                 .padding(.horizontal, .large)
+                .asButton {
+                    
+                }
             
         }
         
@@ -143,6 +170,67 @@ fileprivate struct HeaderContainer<Content: View, HeaderBackground: View>: View 
             .frame(height: 200)
         
     }
+    
+}
 
+#Preview {
+    
+    StretchyHeaderScrollView {
+        
+        VStack(spacing: .appLarge) {
+            
+            ForEach(0..<10) { _ in
+                
+                RoundedRectangle(cornerRadius: .appSmall)
+                    .fill(.indigo)
+                    .frame(height: 100)
+                    .padding(.horizontal, .large)
+                    .asButton {
+                        
+                    }
+                
+            }
+            
+        }
+        .padding(.top, .appMedium)
+        
+    } headerBackground: {
+        
+        Image(.image3)
+            .resizable()
+            .scaledToFill()
+            .padding(.bottom, 200)
+        
+
+//        LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        
+    } headerContent: {
+        
+        VStack {
+            
+            UnevenRoundedRectangle(
+                cornerRadii: .init(
+                    topLeading: 0,
+                    bottomLeading: 24,
+                    bottomTrailing: 24,
+                    topTrailing: 0
+                )
+            )
+                .fill(.green)
+                .frame(height: 200)
+                .opacity(0.8)
+                .overlay {
+                    
+                    Text("Some Text")
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 12)
+                        .appFont(with: .title(.t5))
+                    
+                }
+            
+        }
+        .padding(.top, 225)
+        
+    }
     
 }
