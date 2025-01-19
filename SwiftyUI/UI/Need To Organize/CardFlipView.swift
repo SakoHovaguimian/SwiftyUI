a54346a291483a3c98901b2f80c1c52e9f3c94e1
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2025-01-19 09:53:28 -0800

    custom flip view

--- SwiftyUI/UI/Need To Organize/CardFlipView.swift
+++ SwiftyUI/UI/Need To Organize/CardFlipView.swift
@@ -86,9 +86,9 @@ struct FlipView: View {
     @State var frontDegree = -90.0
     @State var isFlipped = false
     
-    let width : CGFloat = 200
-    let height : CGFloat = 250
-    let durationAndDelay : CGFloat = 0.3
+    let width: CGFloat = 200
+    let height: CGFloat = 250
+    let durationAndDelay: CGFloat = 0.3
     
     var body: some View {
         
@@ -117,20 +117,142 @@ struct FlipView: View {
         self.isFlipped = !isFlipped
         
         if self.isFlipped {
+            
             withAnimation(.linear(duration: self.durationAndDelay)) {
                 self.backDegree = 90
             }
             withAnimation(.linear(duration: self.durationAndDelay).delay(self.durationAndDelay)){
                 self.frontDegree = 0
             }
+            
         } else {
+            
             withAnimation(.linear(duration: self.durationAndDelay)) {
                 self.frontDegree = -90
             }
             withAnimation(.linear(duration: self.durationAndDelay).delay(self.durationAndDelay)){
                 self.backDegree = 0
             }
+            
         }
+        
     }
+    
+}
+
+
+struct AppFlipView<Front: View, Back: View>: View {
+    
+    @State var isFlipped = false
+    @State var backDegree: CGFloat
+    @State var frontDegree: CGFloat
+    
+    private var front: Front
+    private var back: Back
+    private var duration: CGFloat = 0.3
+    
+    init(isFlipped: Bool = false,
+         duration: CGFloat = 0.3,
+         @ViewBuilder front: () -> Front,
+         @ViewBuilder back: () -> Back) {
+        
+        self.duration = duration
+        
+        self.front = front()
+        self.back = back()
+        
+        self._isFlipped = State(initialValue: isFlipped)
+        
+        let degrees = AppFlipView.calculateInitialDegrees(isFlipped: isFlipped)
+        self._frontDegree = State(initialValue: degrees.front)
+        self._backDegree = State(initialValue: degrees.back)
+        
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            
+            back
+                .rotation3DEffect(
+                    Angle(degrees: backDegree),
+                    axis: (x: 0, y: 1, z: 0)
+                )
+            
+            front
+                .rotation3DEffect(
+                    Angle(degrees: frontDegree),
+                    axis: (x: 0, y: 1, z: 0)
+                )
+        
+        }.appOnTapGesture {
+            flipCard()
+        }
+        
+    }
+    
+    func flipCard () {
+        
+        if self.isFlipped {
+            
+            withAnimation(.linear(duration: self.duration)) {
+                self.backDegree = 90
+            }
+            withAnimation(.linear(duration: self.duration).delay(self.duration)){
+                self.frontDegree = 0
+            }
+            
+        } else {
+            
+            withAnimation(.linear(duration: self.duration)) {
+                self.frontDegree = -90
+            }
+            withAnimation(.linear(duration: self.duration).delay(self.duration)){
+                self.backDegree = 0
+            }
+            
+        }
+        
+        self.isFlipped = !self.isFlipped
+        
+    }
+    
+    static func calculateInitialDegrees(isFlipped: Bool) -> (front: CGFloat, back: CGFloat) {
+        
+        if isFlipped {
+            return (front: -90, back: 0)
+        }
+        else {
+            return (front: 0, back: 90)
+        }
+        
+    }
+    
+}
+
+#Preview {
+    
+    AppFlipView() {
+        
+        AppCardView {
+            
+            Text("Front")
+                .appFont(with: .title(.t6))
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+        }
+        
+    } back: {
+        
+        AppCardView {
+            
+            Text("Back")
+                .appFont(with: .title(.t6))
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+        }
+        
+    }
+    .padding(.horizontal, 32)
     
 }
