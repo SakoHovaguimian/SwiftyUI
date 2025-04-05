5c1f45367df5ea208bfd6dc03826d13e316bbd9a
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2025-04-04 18:19:28 -0700

    fixed app not compiling

--- SwiftyUI/ThemeType.swift
+++ SwiftyUI/ThemeType.swift
@@ -1,8 +1,16 @@
 //
-//  DesignSystemWorking.swift
+//  ThemeType.swift
 //  SwiftyUI
 //
-//  Created by Sako Hovaguimian on 4/3/25.
+//  Created by Sako Hovaguimian on 4/4/25.
+//
+
+
+//
+//  NewTheme.swift
+//  Rune
+//
+//  Created by Sako Hovaguimian on 4/4/25.
 //
 
 import SwiftUI
@@ -38,38 +46,47 @@ struct CustomTheme: Equatable, Identifiable, ThemeType {
     let fonts: Fonts
 
     struct Background {
+        
         let primary: AppBackgroundStyle
         let secondary: AppBackgroundStyle
         let tertiary: AppBackgroundStyle
         let quad: AppBackgroundStyle
         
         func value(for key: BackgroundKey) -> AppBackgroundStyle {
+            
             switch key {
             case .primary: return primary
             case .secondary: return secondary
             case .tertiary: return tertiary
             case .quad: return quad
             }
+            
         }
+        
     }
 
     struct TextColor {
+        
         let title: AppForegroundStyle
         let titleInverted: AppForegroundStyle
         let body: AppForegroundStyle
         let bodyInverted: AppForegroundStyle
         
         func value(for key: TextColorKey) -> AppForegroundStyle {
+            
             switch key {
             case .title: return title
             case .titleInverted: return titleInverted
             case .body: return body
             case .bodyInverted: return bodyInverted
             }
+            
         }
+        
     }
 
     struct Spacing {
+        
         let extraSmall: CGFloat
         let small: CGFloat
         let small2: CGFloat
@@ -79,6 +96,7 @@ struct CustomTheme: Equatable, Identifiable, ThemeType {
         let extraLarge: CGFloat
 
         func value(for key: SpacingKey) -> CGFloat {
+            
             switch key {
             case .extraSmall: return extraSmall
             case .small: return small
@@ -89,10 +107,13 @@ struct CustomTheme: Equatable, Identifiable, ThemeType {
             case .extraLarge: return extraLarge
             case .custom(let value): return value
             }
+            
         }
+        
     }
 
     struct Radius {
+        
         let extraSmall: CGFloat
         let small: CGFloat
         let small2: CGFloat
@@ -102,6 +123,7 @@ struct CustomTheme: Equatable, Identifiable, ThemeType {
         let extraLarge: CGFloat
 
         func value(for key: RadiusKey) -> CGFloat {
+            
             switch key {
             case .extraSmall: return extraSmall
             case .small: return small
@@ -112,51 +134,90 @@ struct CustomTheme: Equatable, Identifiable, ThemeType {
             case .extraLarge: return extraLarge
             case .custom(let value): return value
             }
+            
         }
+        
     }
 
     struct Fonts {
+        
         let title: Font
         let subtitle: Font
         let body: Font
         let caption: Font
 
         func value(for key: FontKey) -> Font {
+            
             switch key {
             case .title: return title
             case .subtitle: return subtitle
             case .body: return body
             case .caption: return caption
             }
+            
         }
+        
     }
     
     enum BackgroundKey {
-        case primary, secondary, tertiary, quad
+        
+        case primary
+        case secondary
+        case tertiary
+        case quad
+        
     }
-
+    
     enum SpacingKey {
-        case extraSmall, small, small2, medium, medium2, large, extraLarge
+        
+        case extraSmall
+        case small
+        case small2
+        case medium
+        case medium2
+        case large
+        case extraLarge
         case custom(CGFloat)
+        
     }
-
+    
     enum RadiusKey {
-        case extraSmall, small, small2, medium, medium2, large, extraLarge
+        
+        case extraSmall
+        case small
+        case small2
+        case medium
+        case medium2
+        case large
+        case extraLarge
         case custom(CGFloat)
+        
     }
-
+    
     enum FontKey {
-        case title, subtitle, body, caption
+        
+        case title
+        case subtitle
+        case body
+        case caption
+        
     }
     
     enum TextColorKey {
-        case title, titleInverted, body, bodyInverted
+        
+        case title
+        case titleInverted
+        case body
+        case bodyInverted
+        
     }
+    
 }
 
 // MARK: - Default Theme
 
 extension CustomTheme {
+    
     static let `default` = CustomTheme(
         id: "DEFAULT",
         accentColor: .color(.blue),
@@ -190,7 +251,7 @@ extension CustomTheme {
         background: .init(
             primary: .color(.black),
             secondary: .color(.gray),
-            tertiary: .color(.darkBlue),
+            tertiary: .color(.blue),
             quad: .color(.init(white: 0.1))
         ),
         textColor: .init(
@@ -208,38 +269,60 @@ extension CustomTheme {
             caption: .system(size: 11, weight: .light)
         )
     )
+    
 }
 
 extension CustomTheme.Spacing {
+    
     static let `default` = CustomTheme.Spacing(
-        extraSmall: 4, small: 8, small2: 12, medium: 16,
-        medium2: 20, large: 24, extraLarge: 32
+        extraSmall: 4,
+        small: 8,
+        small2: 12,
+        medium: 16,
+        medium2: 20,
+        large: 24,
+        extraLarge: 32
     )
+    
 }
 
 extension CustomTheme.Radius {
+    
     static let `default` = CustomTheme.Radius(
-        extraSmall: 2, small: 4, small2: 6, medium: 8,
-        medium2: 10, large: 12, extraLarge: 16
+        extraSmall: 2,
+        small: 4,
+        small2: 6,
+        medium: 8,
+        medium2: 10,
+        large: 12,
+        extraLarge: 16
     )
+    
 }
 
 // MARK: - DesignSystem & Environment
 
 protocol DesignSystemProviding: AnyObject {
+    
     var theme: any ThemeType { get }
+    
     func toggleTheme()
+    
     func background(_ key: CustomTheme.BackgroundKey) -> AppBackgroundStyle
     func spacing(_ key: CustomTheme.SpacingKey) -> CGFloat
     func radius(_ key: CustomTheme.RadiusKey) -> CGFloat
     func font(_ key: CustomTheme.FontKey) -> Font
     func textColor(_ key: CustomTheme.TextColorKey) -> AppForegroundStyle
+    
     var accent: AppForegroundStyle { get }
     var titleTextColor: AppForegroundStyle { get }
+    
 }
 
 @Observable
 class DesignSystemProvider: DesignSystemProviding {
+    
+    static let shared = DesignSystemProvider()
     
     var theme: any ThemeType = CustomTheme.default
 
@@ -272,61 +355,49 @@ class DesignSystemProvider: DesignSystemProviding {
     
 }
 
-struct DesignSystemKey: EnvironmentKey {
-    static let defaultValue = DesignSystemProvider()
-}
-
 extension EnvironmentValues {
-    var designSystem: DesignSystemProvider {
-        get { self[DesignSystemKey.self] }
-        set { self[DesignSystemKey.self] = newValue }
-    }
+    @Entry var designSystem = DesignSystemProvider.shared
 }
 
 // MARK: - Demo View
 
-struct ThemeDemoView: View {
-    @Environment(\.designSystem) private var theme
-
-    var body: some View {
-        VStack(spacing: theme.spacing(.medium)) {
-            Text("Design System Theme")
-                .font(theme.font(.title))
-                .foregroundStyle(theme.theme.textColor.title.foregroundStyle())
-                .contentTransition(.interpolate)
-
-            Button("Toggle Theme") {
-                theme.toggleTheme()
-            }
-            .padding(theme.spacing(.small))
-            .background(theme.accent.foregroundStyle())
-            .clipShape(.rect(cornerRadius: theme.radius(.small2)))
-            .foregroundColor(.white)
-            .contentTransition(.interpolate)
-        }
-        .padding(theme.spacing(.large))
-        .background(theme.background(.primary).backgroundStyle())
-        .clipShape(.rect(cornerRadius: theme.radius(.medium2)))
-        .compositingGroup()
-        .contentTransition(.interpolate)
-        .animation(.easeOut(duration: 1.2), value: self.theme.theme.id)
-    }
-}
-
-// MARK: - Entry
 
 @main
 struct ThemeApp: App {
-    @State private var designSystem = DesignSystemProvider()
-
+    
+    @Environment(\.designSystem) private var theme
+    
     var body: some Scene {
         
         WindowGroup {
             
-            ThemeDemoView()
-                .environment(\.designSystem, designSystem)
+            VStack(spacing: theme.spacing(.medium)) {
+                
+                Text("Design System Theme")
+                    .font(theme.font(.title))
+                    .foregroundStyle(theme.theme.textColor.title.foregroundStyle())
+                    .contentTransition(.interpolate)
+                
+                Button("Toggle Theme") {
+                    theme.toggleTheme()
+                }
+                .padding(theme.spacing(.small))
+                .background(theme.accent.foregroundStyle())
+                .clipShape(.rect(cornerRadius: theme.radius(.small2)))
+                .foregroundColor(.white)
+                .contentTransition(.interpolate)
+                
+            }
+            .padding(theme.spacing(.large))
+            .background(theme.background(.primary).backgroundStyle())
+            .clipShape(.rect(cornerRadius: theme.radius(.medium2)))
+            .compositingGroup()
+            .contentTransition(.interpolate)
+            .animation(.easeOut(duration: 1.2), value: self.theme.theme.id)
             
         }
         
     }
+    
 }
+
