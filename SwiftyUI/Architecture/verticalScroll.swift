27f4d439dfeffcd2f47f04ad09769cfeed938240
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2025-04-14 20:33:17 -0700

    POC theme system

--- SwiftyUI/Architecture/verticalScroll.swift
+++ SwiftyUI/Architecture/verticalScroll.swift
@@ -38,123 +38,144 @@ struct VerticalScrollView: View {
     
     var body: some View {
         
-        return ZStack {
-            
-            Color(uiColor: .systemGray6)
-                .ignoresSafeArea()
-            
-            ScrollView {
-                
-                VStack(spacing: 0) {
-                    
-                    ForEach(pages, id: \.self) { page in
-                        
-                        VStack {
-                            
-                            Image(.image3)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .clipShape(.rect(cornerRadius: 12))
-                                .padding(.horizontal, 24)
-                            
-                        }
-                        .frame(
-                            width: UIScreen.main.bounds.width,
-                            height: UIScreen.main.bounds.height
-                        )
-                        .background(.red)
-                        
-                    }
-
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .scrollTargetLayout()
-                
-            }
-            .scrollTargetBehavior(.paging)
-            .scrollPosition(id: self.$currentPage, anchor: .top)
-            .defaultScrollAnchor(.top)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .ignoresSafeArea()
+        scrollView()
             .overlay(alignment: .top) {
-                
-                VStack(spacing: 32) {
-                    
-                    if currentPage == .one,
-                       let header = header(currentPage) {
-                                                                        
-                        Text(header.title)
-                            .frame(maxWidth: .infinity)
-                            .transition(.opacity.combined(with: .blurReplace))
-                        
-                        Text(header.message)
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal, 32)
-                            .multilineTextAlignment(.center)
-                            .transition(.opacity.combined(with: .blurReplace))
-    
-                    }
-                    
-                    if currentPage == .two,
-                       let header = header(currentPage) {
-                                                                        
-                        Text(header.title)
-                            .frame(maxWidth: .infinity)
-                            .transition(.opacity.combined(with: .blurReplace))
-                        
-                        Text(header.message)
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal, 32)
-                            .multilineTextAlignment(.center)
-                            .transition(.opacity.combined(with: .blurReplace))
-    
-                    }
-                    
-                    if currentPage == .three,
-                       let header = header(currentPage) {
-                                                                        
-                        Text(header.title)
-                            .frame(maxWidth: .infinity)
-                            .transition(.opacity.combined(with: .blurReplace))
-                        
-                        Text(header.message)
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal, 32)
-                            .multilineTextAlignment(.center)
-                            .transition(.opacity.combined(with: .blurReplace))
-    
-                    }
-                    
-                }
-                .padding(.bottom, 12)
-                .background(Color(uiColor: .systemGray6))
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .animation(.spring(), value: self.currentPage)
-                
+                header()
             }
             .safeAreaInset(edge: .bottom) {
+                button()
+            }
+        
+    }
+    
+    private func scrollView() -> some View {
+        
+        ScrollView {
+            
+            VStack(spacing: 0) {
                 
-                AppButton(title: "Finish", titleColor: .white, backgroundColor: .green) {}
-                    .opacity(self.currentPage == .three ? 1 : 0)
-                    .offset(y: self.currentPage == .three ? 0 : 200)
-                    .animation(.smooth, value: self.currentPage)
-                    .padding(.horizontal, 32)
+                ForEach(pages, id: \.self) { page in
+                    
+                    VStack {
+                        
+                        Image(.image3)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(.rect(cornerRadius: 12))
+                            .padding(.horizontal, 24)
+                        
+                    }
+                    .frame(
+                        width: UIScreen.main.bounds.width,
+                        height: UIScreen.main.bounds.height
+                    )
+                    
+                }
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .scrollTargetLayout()
+            
+        }
+        .background(.mint)
+        .scrollTargetBehavior(.paging)
+        .scrollPosition(id: self.$currentPage, anchor: .top)
+        .defaultScrollAnchor(.top)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        
+    }
+    
+    private func header() -> some View {
+        
+        VStack(spacing: 32) {
+            
+            let item = getHeaderCopy(self.currentPage)
+            
+            if self.currentPage == .one {
+                
+                Text(item?.title ?? "N/A")
+                    .frame(maxWidth: .infinity)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .trailing)),
+                        removal: .opacity
+                    ))
+                
+                Text(item?.message ?? "N/A")
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 16)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .leading)),
+                        removal: .opacity
+                    ))
+                
+            } else if self.currentPage == .two {
+                
+                Text(item?.title ?? "N/A")
+                    .frame(maxWidth: .infinity)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .trailing)),
+                        removal: .opacity
+                    ))
+                
+                Text(item?.message ?? "N/A")
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: .infinity)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .leading)),
+                        removal: .opacity
+                    ))
+                
+            } else if currentPage == .three {
+                
+                Text(item?.title ?? "N/A")
+                    .frame(maxWidth: .infinity)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .trailing)),
+                        removal: .opacity
+                    ))
+                
+                Text(item?.message ?? "N/A")
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: .infinity)
+                    .transition(.asymmetric(
+                        insertion: .opacity.combined(with: .move(edge: .leading)),
+                        removal: .opacity
+                    ))
                 
             }
             
+        }
+        .padding(.vertical, 24)
+        .background(.mint)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .animation(.easeInOut(duration: 0.6), value: self.currentPage)
+        .allowsHitTesting(false)
+        
+    }
+    
+    func getHeaderCopy(_ currentPage: Page?) -> (title: String, message: String)? {
+        
+        guard let currentPage else { return ("", "") }
+        
+        switch currentPage {
+        case .one: return (currentPage.title, currentPage.longPageMessage)
+        case .two: return (currentPage.title, currentPage.longPageMessage)
+        case .three: return (currentPage.title, currentPage.longPageMessage)
         }
         
-        func header(_ currentPage: Page?) -> (title: String, message: String)? {
-            
-            guard let currentPage else { return ("", "") }
-            
-            switch currentPage {
-            case .one: return (currentPage.title, currentPage.longPageMessage)
-            case .two: return (currentPage.title, currentPage.longPageMessage)
-            case .three: return (currentPage.title, currentPage.longPageMessage)
-            }
-            
-        }
+    }
+    
+    private func button() -> some View {
+        
+        AppButton(title: "Finish", titleColor: .white, backgroundColor: .mint) {}
+            .opacity(self.currentPage == .three ? 1 : 0)
+            .offset(y: self.currentPage == .three ? 0 : 200)
+            .animation(.smooth, value: self.currentPage)
+            .padding(.horizontal, 32)
         
     }
     
